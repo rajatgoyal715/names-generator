a18f2c763cbea38100337b10c456d2df161894e9
Rajat Goyal <rajatgoyal715@gmail.com>
2023-08-11 23:28:33 +0530

    Set text style

--- lib/main.dart
+++ lib/main.dart
@@ -69,12 +69,15 @@ class BigCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
+    var style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary
+    );
 
     return Card(
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase),
+        child: Text(pair.asLowerCase, style: style),
       ),
     );
   }
