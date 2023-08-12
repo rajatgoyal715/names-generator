0b3fec65847bcb2c12a0de3727d30a0545c839e4
Rajat Goyal <rajatgoyal715@gmail.com>
2023-08-12 10:34:29 +0530

    Create a Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,14 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,9 +71,12 @@ class MyHomePage extends StatelessWidget {
                   label: Text('Favorites'),
                 ),
               ],
-              selectedIndex: 0,
+              selectedIndex: selectedIndex,
               onDestinationSelected: (value) {
                 print('selected: $value');
+                setState(() {
+                  selectedIndex = value;
+                });
               },
             )
           ),
